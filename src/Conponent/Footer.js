d214081382fc46363d5aef67cadf315dd8ad8c2f
Ankit_Kishore_Khalkho <ankitjasonxalxo9@gmail.com>
2022-03-06 23:10:46 +0530

    Changed footer

--- src/Conponent/Footer.js
+++ src/Conponent/Footer.js
@@ -1,130 +1,103 @@
 import React from "react";
 import "./footer.css";
 import { Link } from "react-router-dom";
-
+import {
+  FaFacebook,
+  FaTelegramPlane,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa";
 const Footer = () => {
+  
+
   return (
-    <div>
-      <div className="full">
-        <div className="cover">
-          <div className="row text-center">
-            <div className="col-sm compLinkas">
-              <ul>
-                <li>
-                  <h4 className="mar">Components</h4>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/">
-                    Home
-                  </Link>
-                </li>
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-10 "></div>
+            <div className="row">
+              <div className="col-6 col-lg-4 foot ">
+                <h2>Links</h2>
+                <ul className="lit">
+                  <li>
+                    <Link className="linkcolor" as={Link} to="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="linkcolor"
+                      as={Link}
+                      to="/Eventdiscription"
+                    >
+                      Events
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="linkcolor" as={Link} to="/GalleryFullPage">
+                      Gallery
+                    </Link>
+                  </li>
+                </ul>
+              </div>
 
-                <li>
-                  <Link className="linkcolor" as={Link} to="/AboutPage">
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/Eventsdiscription">
-                    Events
-                  </Link>
-                </li>
-              </ul>
+              <div className="col-6 col-lg-4 foot">
+                <h2>More</h2>
+                <ul className="lit">
+                  <li>
+                    <Link className="linkcolor" as={Link} to="/Members">
+                      Members
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="linkcolor" as={Link} to="/ContactUs">
+                      contact Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="linkcolor" as={Link} to="/AboutPage">
+                      About
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-12 col-lg-4 foot">
+                <h2>Follow</h2>
+                <div className="row">
+                  <div className="col-3 social ">
+                    <a href="https://www.facebook.com/bitsnss/">
+                      <FaFacebook size="2.9rem" color="white" />
+                    </a>
+                  </div>
+                  <div className="col-3 social">
+                    <a href="https://t.me/nssbitsindri">
+                      <FaTelegramPlane size="3rem" color="white" />
+                    </a>
+                  </div>
+                  <div className="col-3 social">
+                    <a href="https://www.instagram.com/nss_bitsindri/?hl=en">
+                      <FaInstagram size="3rem" color="white" />
+                    </a>
+                  </div>
+                  <div className="col-3 social">
+                    <a href="https://www.youtube.com/channel/UC7dOKpb4htlwViLRfHdH8Vw">
+                      <FaYoutube size="3rem" color="white" />
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col-sm compLinkas">
-              <ul>
-                <li>
-                  <h4 className="mar">Components</h4>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/GalleryFullPage">
-                    Gallery
-                  </Link>
-                </li>
 
-                <li>
-                  <Link className="linkcolor" as={Link} to="/ContactUs">
-                    contact Us
-                  </Link>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/Members">
-                    Members
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div class="col-sm compLinkas">
-              <ul>
-                <li>
-                  <h4 className="mar">Components</h4>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/TermAndCondition">
-                    Term and condition
-                  </Link>
-                </li>
-
-                <li>
-                  <Link className="linkcolor" as={Link} to="/Policy">
-                    Pravecy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link className="linkcolor" as={Link} to="/WebTechMembers">
-                    Web Tech Members
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <br />
-          <br />
-
-          <br></br>
-          <div className="sizeofdiv text-center">
-            <div className="sizeofdiv row text-center">
-              <div className="col">
-                <a
-                  className="linkcolor"
-                  href="https://www.facebook.com/bitsnss/"
-                >
-                  <div className="colcircle">
-                    <img src="images/fb.png" alt="socialImg"></img>
-                  </div>
-                </a>
-              </div>
-
-              <div className="col">
-                <a
-                  className="linkcolor"
-                  href="https://www.instagram.com/nss_bitsindri/?hl=en"
-                >
-                  <div className="colcircle ">
-                    <img src="images/insta.png" alt="socialImg"></img>
-                  </div>
-                </a>
-              </div>
-              <div className="col">
-                <a href="https://www.instagram.com/nss_bitsindri/?hl=en">
-                  <div className="colcircle">
-                    <img src="images/twitter.png" alt="socialImg"></img>
-                  </div>
-                </a>
-              </div>
-              <div className="col">
-                <a href="https://www.youtube.com/channel/UC7dOKpb4htlwViLRfHdH8Vw ">
-                  <div className="colcircle">
-                    <img src="images/youtube.png" alt="socialImg"></img>
-                  </div>
-                </a>
-              </div>
+            <div className="mt-5">
+              <p className=" text-center  copy">
+                Copyright ©️ NSS BIT SINDRI | All rights reserved.
+              </p>
             </div>
           </div>
-          <div className="copyright">@copyright</div>
         </div>
-      </div>
-    </div>
+      </footer>
+    </>
   );
 };
 
