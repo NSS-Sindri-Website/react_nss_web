b1000281b17d6a76edc30f03952016bf05b4d7c2
Ankit_Kishore_Khalkho <ankitjasonxalxo9@gmail.com>
2022-12-01 22:52:52 +0530

    Fixed navbar responsiveness for mobile

--- src/Conponent/Navbar.js
+++ src/Conponent/Navbar.js
@@ -42,60 +42,158 @@ function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="toggleMobileMenu">
+          <div className="collapse navbar-collapse " id="toggleMobileMenu">
             <ul className="navbar-nav text-center ms-auto">
               <li>
-                <Link as={Link} to="/" className="nav-link">
+                <Link
+                  as={Link}
+                  to="/"
+                  className="nav-link"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   Home
                 </Link>
               </li>
 
               <NavDropdown title="Members" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/M17" href="#m1">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/M17"
+                  href="#m1"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2017</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/M18" href="#m2">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/M18"
+                  href="#m2"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2018</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/M19" href="#m3">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/M19"
+                  href="#m3"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2019</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/M20" href="#m4">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/M20"
+                  href="#m4"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2020</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/M21" href="#m5">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/M21"
+                  href="#m5"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2021</p>
                 </NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Events" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/E23" href="#e1">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/E23"
+                  href="#e1"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2022-23</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/E22" href="#e1">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/E22"
+                  href="#e1"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2021-22</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/E21" href="#e1">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/E21"
+                  href="#e1"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks"> 2020-21</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/E20" href="#e2">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/E20"
+                  href="#e2"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks">2019-20</p>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/E19" href="#e3">
+                <NavDropdown.Item
+                  as={Link}
+                  to="/E19"
+                  href="#e3"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   <p className="droplinks">2018-19</p>
                 </NavDropdown.Item>
               </NavDropdown>
               <li>
-                <Link as={Link} to="/GalleryFullPage" className="nav-link">
+                <Link
+                  as={Link}
+                  to="/GalleryFullPage"
+                  className="nav-link"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   Gallery
                 </Link>
               </li>
               <li>
-                <Link as={Link} to="/AboutPage" className="nav-link">
+                <Link
+                  as={Link}
+                  to="/AboutPage"
+                  className="nav-link"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   About Us
                 </Link>
               </li>
               <li>
-                <Link as={Link} to="/ContactUs" className="nav-link">
+                <Link
+                  as={Link}
+                  to="/ContactUs"
+                  className="nav-link"
+                  onClick={() => {
+                    ".navbar-collapse".collapse("hide");
+                  }}
+                >
                   Contact Us
                 </Link>
               </li>
