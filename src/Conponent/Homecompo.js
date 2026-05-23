76756dccf81cbbb05bfcaffa76fd9fec73a4e496
nss <nssbitsindri@gmail.com>
2026-05-23 17:48:41 +0530

    updated_home4

--- src/Conponent/Homecompo.js
+++ src/Conponent/Homecompo.js
@@ -1,33 +1,23 @@
-// import React from "react";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
 // import "../modal.css";
 // import Modal from "../Modal";
+
 import NewCarousel from "../NewCarousel";
 // import Carousel from "./Carousel";
+
 import About from "./About";
 import Gallery from "./Gallery";
 // import Events from "./Events";
+
 import PostBearer from "./PostBearer";
 import EventsHome from "./EventsHome";
 import Testimonial from "./Testimonial";
 
 function Homecompo() {
-  //pop modal 
-  // const [openModal, setOpenModal] = useState(false);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setOpenModal(true);
-  //   }, 3000);
-  // }, []);
   return (
     <div>
-      {/* popup modal */}
-      {/* <Modal open={openModal} onClose={() => setOpenModal(false)} /> */}
       <NewCarousel />
       <About />
-      {/* <Events /> */}
       <EventsHome />
       <Testimonial />
       <PostBearer />
